4148d5461c6c710d3a2cf6366f011b40289ab043
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-03-20 21:30:35 +0100

    refactor: remove unnecessary lazy loading

--- createComponent.jsx
+++ createComponent.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 function createComponent(name, base64) {
-  return ({ src, alt, loading, ...rest }) => (
+  return ({ src, alt, ...rest }) => (
     <img
       src={`data:image/png;base64,${base64}`}
-      loading={loading ?? 'lazy'}
       alt={name}
       {...rest}
     />
